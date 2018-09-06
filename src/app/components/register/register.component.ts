38680cb1139d85803c1769b5fecb9346129049cb
estherkoko <estherkolade@gmail.com>
2018-09-06 00:37:19 -0400

    added register forms

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  fullname: String;
+  username: String;
+  email: String;
+  password : String;
   constructor() { }
 
   ngOnInit() {
