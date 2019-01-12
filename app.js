ecbed72d802e1e0a0595343b00343feb1b3b435f
estherkoko <estherkolade@gmail.com>
2019-01-12 00:02:33 -0500

    dist

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 //Set static folder
-app.use(express.static(path.join(__dirname, 'dist/chatservice')));
+//app.use(express.static(path.join(__dirname, 'dist/chatservice')));
 app.get('*', function(req, res) {
   res.sendFile('dist');
 });
