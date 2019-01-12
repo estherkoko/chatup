230fdd8c555a76804301ca3b9a18dba2307f457e
estherkoko <estherkolade@gmail.com>
2019-01-12 00:50:21 -0500

    dist

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use('/api/messages', messages);
 //index route
 
 app.get('*',function(req,res){
-  res.sendFile(__dirname + '/chatservice/src/index.html'); 
+  res.sendFile(__dirname + '/dist/chatservice/index.html'); 
 });
 /* socket stuf */
 function handler (req, res) {
